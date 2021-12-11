b4b9e7a7e36c2194b34060ffe60f85c6976a078f
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2021-12-12 00:48:37 +0200

    removing unused vars

--- src/Layout/body/body.jsx
+++ src/Layout/body/body.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import Card from '../../components/card/card'
 import './body.sass'
-import { importAll } from '../../utils/helpers.js'
  
 const Body = (props) => {
   const {children, results} = props
-  console.log(`Body: `, results)
- 
   const gameCards = results?.map((data) => <Card key={data.id} data={data}/> )
  
   return (
